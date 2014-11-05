88ad91332932ed929e5a22894b1ef537d94ddac9
Jason Carter <carter.jk@gmail.com>
2014-11-04 21:28:05 -0600

    Simple demonstration of an MSTest unit test that runs fine in Visual Studio 2010-2013's test engines, as well as Resharper's test rummer, but fails in NCrunch

---
+++ DeploymentItemTest/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DeploymentItemTest
+{
+    [TestClass]
+    [DeploymentItem(@"SubFolder\TextFile1.txt")]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange
+            const string expectedContents = "Contents";
+
+            // Act
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"TextFile1.txt");
+            string actualContents = File.ReadAllText(filePath);
+
+            // Assert
+            Assert.AreEqual(expectedContents, actualContents);
+        }
+    }
+}
